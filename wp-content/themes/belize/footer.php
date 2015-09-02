d2ebd92a34951bd563bb2b0fe36a758e89aded3a
John Yoder <johngilesyoder@gmail.com>
2015-09-02 13:59:07 -0600

    initial commit

---
+++ wp-content/themes/belize/footer.php
@@ -0,0 +1,17 @@
+		<footer class="site-footer">
+  		<div class="footer-links">
+  			<nav class="links-column">
+  				<?php footer_links_1(); ?>
+  			</nav>
+  		</div>
+      <div class="footer-company">
+      	<div class="container">
+        	<span>© Glovers Reef Island <?php echo date('Y'); ?>. </span> All rights reserved.
+      	</div>
+      </div>
+    </footer>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ wp-content/themes/belize/template-home.php
@@ -0,0 +1,5 @@
+<?php /* Template Name: Home Page Template */ get_header(); ?>
+
+  HOME
+
+<?php get_footer(); ?>
