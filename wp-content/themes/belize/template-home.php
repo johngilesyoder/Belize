c4b1987a5b4e215a565ddad3263e546b03cfa543
John Yoder <johngilesyoder@gmail.com>
2015-09-09 18:25:43 -0600

    Copy revisions

--- wp-content/themes/belize/template-home.php
+++ wp-content/themes/belize/template-home.php
@@ -17,7 +17,7 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/feature-location.jpg">
           <h3>Location</h3>
           <p>
-            Exotic private island in Glover’s Reef Atoll on Belize’s Barrier Reef—the most remote and unspoiled atolls found in Belize and a designated UNESCO World Heritage Site.
+            Exotic private island in Glover’s Reef Atoll on Belize’s Barrier Reef—the most remote and unspoiled atoll in Belize and a designated UNESCO World Heritage Site.
           </p>
         </div>
       </div>
@@ -26,7 +26,7 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/feature-activities.jpg">
           <h3>Activities</h3>
           <p>
-            The island offers all inclusive, guided tour packages including: sea and surf kayaking, surfing, paddle-boarding, kite and wind surfing, snorkeling, scuba diving, kayak fishing and rainforest and archeological trips to the mainland.
+            All inclusive, guided tour packages including: sea and surf kayaking, surfing, paddle-boarding, kite and wind surfing, snorkeling, scuba diving, kayak fishing and rainforest and archeological trips to the mainland.
           </p>
         </div>
       </div>
@@ -35,7 +35,7 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/feature-business.jpg">
           <h3>Business</h3>
           <p>
-            This highly reputable and profitable turnkey adventure travel business was the first sea kayak outfitter in Belize has been in operation for 38 years.
+            This highly reputable and profitable turnkey adventure travel business was the first sea kayak outfitter in Belize and has been in operation for 38 years.
           </p>
         </div>
       </div>
@@ -64,7 +64,10 @@
     <div class="row">
       <div class="col-sm-6">
         <p>
-          This turnkey adventure travel business is based on an exotic 13.6-acre private island and is one of five islands found at Glover’s Reef Atoll, the most remote and unspoiled atoll found in Belize. The business includes 5.3 acres on the windward side of the island bordered both the open Caribbean to the southeast and a quiet lagoon to the northwest. Glover’s Reef is part of the most famous of Belize’s natural attractions, the Barrier Reef, which stretches in a line 20 to 25 miles offshore for nearly the entire length of the country. The reef is studded with idyllic tropical islands, with the offshore atolls enclosing the most pristine of the reefs found in the country, including the internationally known Blue Hole.
+          This turnkey adventure travel business is based on an exotic 13.6-acre private island and is one of five islands found at Glover’s Reef Atoll, the most remote and unspoiled atoll in Belize. The business includes 5.3 acres on the windward side of the island bordered both by the open Caribbean to the southeast and a quiet lagoon to the northwest. Glover’s Reef is part of the most famous of Belize’s natural attractions, the Barrier Reef, which stretches in a line 20 to 25 miles offshore for nearly the entire length of the country. The reef is studded with idyllic tropical islands, with the offshore atolls enclosing the most pristine of the reefs found in the country, including the internationally known Blue Hole.
+        </p>
+        <p>
+          The atoll encompasses one of Belize’s protected National Marine Reserves, and has been recognized for its outstanding beauty and pristine condition as a designated UNESCO World Heritage Site. The surrounding reefs, open Caribbean sea, and palm covered beaches rival any tropical island in the Caribbean. Belize is world famous for its scuba diving and pristine barrier reef. It is a truly world class location and boasts the best coral reefs in the western hemisphere.
         </p>
       </div>
       <div class="col-sm-6">
@@ -77,13 +80,13 @@
           </div>
           <aside id="bubbles-aside" class="aside-desktop">
             <p>
-              <strong>GLOVER’S REEF ATOLL</strong> &mdash; ONE OF THE MOST PRISTINE ATOLLS IN BELIZE AND A DESIGNATED WORLD HERITAGE SITE
+              <strong>GLOVER’S REEF ATOLL</strong> &mdash; The atoll encompasses one of Belize’s protected National Marine Reserves, and has been recognized for its outstanding beauty and pristine condition as a designated UNESCO World Heritage Site.
             </p>
           </aside>
         </div>
         <aside class="aside-mobile">
           <p>
-            <strong>GLOVER’S REEF ATOLL</strong> &mdash; ONE OF THE MOST PRISTINE ATOLLS IN BELIZE AND A DESIGNATED WORLD HERITAGE SITE
+            <strong>GLOVER’S REEF ATOLL</strong> &mdash; The atoll encompasses one of Belize’s protected National Marine Reserves, and has been recognized for its outstanding beauty and pristine condition as a designated UNESCO World Heritage Site.
           </p>
         </aside>
       </div>
@@ -132,7 +135,7 @@
         <div class="primary-section">
           <h3 class="primary-title">Adventure Sports &amp; Mainland Tours</h3>
           <p>
-            We have built a reputation of offering high quality trips highlighting professionally run sport activities, specializing in river and sea kayak adventures and extraordinary snorkeling opportunities. Using top-of-the-line equipment and trainers, we offer all inclusive, guided tour packages including: sea and surf kayaking, surfing, paddle boarding, kite and wind surfing, snorkeling and kayak fishing. Guests can scuba dive through a professional PADI dive shop operated by our friends and associates on an adjacent 1.2 acres. In addition to the activities offered on the island, we offer a 3-day trip on the mainland to the with waterfall rappelling and zip-line adventures in the Bocawina rainforest, a kayaking trip on the Mopan River (including an exploration of the limestone caves on the Caves Branch River) and a visit to the Castillo pyramid in the Xunantunich Mayan Ruins.
+            We have built a reputation of offering high quality trips highlighting professionally run sport activities, specializing in river and sea kayak adventures and extraordinary snorkeling opportunities. Using top-of-the-line equipment and trainers, we offer all inclusive, guided tour packages including: sea and surf kayaking, surfing, paddle boarding, kite and wind surfing, snorkeling and kayak fishing. Guests can scuba dive through a professional PADI dive shop operated by our friends and associates on an adjacent 1.2 acres. In addition to the activities offered on the island, we offer a 3-day trip on the mainland with waterfall rappelling and zip-line adventures in the Bocawina rainforest, a kayaking trip on the Mopan River (including an exploration of the limestone caves on the Caves Branch River) and a visit to the Castillo pyramid in the Xunantunich Mayan Ruins.
           </p>
         </div>
       </div>
@@ -166,12 +169,12 @@
     <div class="row">
       <div class="col-md-6">
         <p>
-          This highly reputable and profitable adventure travel business was the first sea kayak outfitter in Belize has been in operation since 1977 and grosses over $1M per year. The company has maintained a reputation for high quality trips and innovative itineraries. The business includes the 5.3-acre island resort and a 4-bed, 3-bath house office in an upscale neighborhood on a canal network one block from the ocean. The building serves as staff quarters, storage for equipment, vehicles and dock for our shuttle boat) in Belize City. Other assets include a 41’ shuttle boat, vehicle fleet for our trips, 37 sea kayaks, 10 surf kayaks, 4 fishing kayaks, 6 surfboards, 13 paddleboards, and 14 windsurf rigs. All reservations and record keeping are processed through the head office in Moab. Support staff includes: office managers, local guides, cooks, maintenance and groundskeepers, and local captains and drivers.
+          This highly reputable and profitable adventure travel business was the first sea kayak outfitter in Belize. It has been in operation since 1977 and grosses over $1M per year. The company has maintained a reputation for high quality trips and innovative itineraries. The business includes the 5.3-acre island resort and a 4-bed, 3-bath house office in Belize City in an upscale neighborhood on a canal network one block from the ocean. The building serves as staff quarters, storage for equipment, vehicles and dock for our shuttle boat. Other assets include a 41’ shuttle boat, vehicle fleet for our trips, 37 sea kayaks, 10 surf kayaks, 4 fishing kayaks, 6 surfboards, 13 paddleboards, and 14 windsurf rigs. All reservations and record keeping are processed through the head office in Moab, Utah. Support staff includes: office managers, local guides, cooks, maintenance and groundskeepers, and local captains and drivers.
         </p>
       </div>
       <div class="col-md-6">
         <p>
-          The island is for sale only as part of a package with the purchase of the business. The business ($995,000) includes over $600,000 in assets, operational office in Moab, UT, website, and trademark. The island property ($1,800,000) includes 5.3 acre island resort and all structural facilities.
+          The island is for sale only as part of a package with the purchase of the business. The business ($995,000) includes over $600,000 in assets, an operational office in Moab, Utah, a website, and a trademark. The island property ($1,800,000) includes 5.3 acre island resort and all structural facilities.
         </p>
         <div id="business-metric" class="business-metric">
           <span class="metric-large"><div id="counter-number">0</div></span>
@@ -185,7 +188,7 @@
       <div class="row">
         <div class="col-md-6">
           <p>
-            Belize is fast becoming known as a top destination in the adventure travel market, with its extensive nature reserves, coral barrier reef, limestone cave system, and jungle habitat. The country also is becoming famous for its archeological treasures from the Mayan civilization, and has already established itself as a world-class scuba diving destination and sport fishing paradise. Its tropical setting, safe political climate, and excellent infrastructure are attracting ever increasing foreign investments, especially in the tourism sector which has seen phenomenal growth in the last five years. A new $40M airport expansion, new $50M Carnival Cruise Line terminal, and the construction of many new hotels and resorts attest to the rapid growth Belize is currently experiencing.
+            Belize is fast becoming known as a top destination in the adventure travel market, with its extensive nature reserves, coral barrier reef, limestone cave system and jungle habitat. The country also is becoming famous for its archeological treasures from the Mayan civilization and has already established itself as a world-class scuba diving destination and sport fishing paradise. Its tropical setting, safe political climate and excellent infrastructure are attracting ever increasing foreign investments, especially in the tourism sector which has seen phenomenal growth in the last five years. A new $40M airport expansion, new $50M Carnival Cruise Line terminal and the construction of many new hotels and resorts attest to the rapid growth Belize is currently experiencing.
           </p>
         </div>
         <div class="col-md-6">
@@ -215,7 +218,7 @@
         <div class="primary-section">
           <h3 class="primary-title">Brimming with exotic life</h3>
           <p>
-            We have built a reputation of offering high quality trips highlighting professionally run sport activities, specializing in river and sea kayak adventures and extraordinary snorkeling opportunities. Using top-of-the-line equipment and trainers, we offer all inclusive, guided tour packages including: sea and surf kayaking, surfing, paddle boarding, kite and wind surfing, snorkeling and kayak fishing. Guests can scuba dive through a professional PADI dive shop operated by our friends and associates on an adjacent 1.2 acres. In addition to the activities offered on the island, we offer a 3-day trip on the mainland to the with waterfall rappelling and zip-line adventures in the Bocawina rainforest, a kayaking trip on the Mopan River (including an exploration of the limestone caves on the Caves Branch River) and a visit to the Castillo pyramid in the Xunantunich Mayan Ruins.
+            According to the Wildlife Conservation Society, "The Belize Barrier reef is one of the world’s outstanding barrier reef systems, containing a necklace of three offshore atolls, hundreds of sand cays and patch reefs, mangrove forests, coastal lagoons, estuaries and a thriving ecosystem comprised of approximately 500 species of fish, 134 bird species, three varieties of nesting sea turtles, and one of the largest populations of West Indian Manatees." Glover's harbors one of the greatest diversity of reef types in the western Caribbean and is considered one of the highest priority areas in the Mesoamerican reef system. Species frequently seen include iguana, geckos, a variety of dolphins, stingrays, turtles, sharks, eels, grunts, wrasse, bone fish, snapper, angel fish, and many other fish species.  Osprey, falcons, herons, nighthawks, flycatchers, warblers, swallows are just some of the bird species found at Glover’s Reef.
           </p>
         </div>
       </div>
